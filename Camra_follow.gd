ee911fd2c68ba18649ff03b7921529cf81765fdc
Joseph Colon <josephmindustry@gmail.com>
2024-02-25 19:23:37 -0500

    Made camra2d scrpit
    Im made a gd script that inherits carma 2d

---
+++ Camra_follow.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# we whant code here to folow the players x cords not its y cords if that makes sense's
+	pass
